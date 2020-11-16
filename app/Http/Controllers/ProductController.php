14f5eb42f75f3515a8b01367601744a9432899ef
Shehzad Rana <shehzadcse@gmail.com>
2020-11-16 22:29:12 +0530

    Completed Lecture 27. Reading and Selecting Data with Models in Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Product;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        // $products = DB::table('products')->get();
+        $products = Product::all();        
+        dd($products);
         return view('products.index');
     }
     public function create()
@@ -22,6 +27,10 @@ class ProductController extends Controller
 
     public function show($product)
     {
+        // $product = DB::table('products')->where('id',$product)->first();
+        // $product = DB::table('products')->where('id',$product)->first();
+        $product = Product::findOrFail($product);        
+        dd($product);
         return view('products.show');
     }
     public function edit($product)
