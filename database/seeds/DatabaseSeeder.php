fa0223d744b6516b0c9dc1498e0a53bccfbf64c0
Shehzad Rana <shehzadcse@gmail.com>
2020-11-16 21:24:07 +0530

    Created Seeder for Products

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);        
+        $faker = Faker\Factory::create();
+        for($i = 0; $i < 200; $i++) {
+            App\Product::create([
+                'title' => $faker->sentence(3),
+                'description' => $faker->paragraph(1),
+                'price' => $faker->randomFloat($maxDecimals=2, $minval = 3,$maxval=100),
+                'stock' => $faker->numberBetween(1,10),
+                'status' => $faker->randomElement(['available','unavailable'])
+            ]);
+        }
     }
 }
