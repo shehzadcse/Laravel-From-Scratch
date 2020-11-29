5a16a6934bc43e23d1ca1775c0c5357289c9120c
Shehzad Rana <shehzadcse@gmail.com>
2020-11-29 13:12:50 +0530

    Test Commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Product;
+
 
 class ProductController extends Controller
 {
     public function index()
     {
-        // $products = DB::table('products')->get();
-        $products = Product::all();        
+        // $products = DB::table('products')->get();        
+        $products = Product::all();
         dd($products);
-        return view('products.index');
+        return view('products.index');        
     }
     public function create()
     {
@@ -22,32 +21,23 @@ class ProductController extends Controller
 
     public function store()
     {
-
     }
-
     public function show($product)
     {
         // $product = DB::table('products')->where('id',$product)->first();
         // $product = DB::table('products')->where('id',$product)->first();
-        $product = Product::findOrFail($product);        
+        $product = Product::findOrFail($product);
         dd($product);
         return view('products.show');
     }
     public function edit($product)
     {
-        return 'A form to edit product '.$product.' from ProductController';
+        return 'A form to edit product ' . $product . ' from ProductController';
     }
-
     public function update($product)
     {
-        
     }
-
     public function destroy($product)
     {
-        
     }
-
 }
-
-
